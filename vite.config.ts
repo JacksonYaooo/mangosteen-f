59b1d503911ced8aacc17f1bd794d593b04e63fd
JacksonYaooo <2594838054@qq.com>
2023-12-19 13:28:20 +0800

    feat: reset base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/mangosteen-f/dist/',
+  // base: '/mangosteen-f/dist/',
   plugins: [vue()]
 })
