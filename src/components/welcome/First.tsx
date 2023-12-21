21e7a382605602cf77fba8f62f83cc4c3843f9af
JacksonYaooo <2594838054@qq.com>
2023-12-21 20:06:10 +0800

    feat: 完成欢迎界面动画

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,24 +1,12 @@
-import { defineComponent } from "vue";
-import s from './WelcomLayout.module.scss'
+import s from './welcome.module.scss';
 import pig from '../../assets/icons/pig.svg'
-import { WelcomLayout } from "./WelcomeLayout";
-import { RouterLink } from "vue-router";
 
-export const First = defineComponent({
-  setup(props, context) {
-    const slots = {
-      icon: () => <img src={pig} />,
-      title: () => <h2>会挣钱<br />还会省钱</h2>,
-      buttons: () => <>
-        <RouterLink class={s.fake} to="/start">跳过</RouterLink>
-        <RouterLink to="/welcome/2">下一页</RouterLink>
-        <RouterLink to="/start">跳过</RouterLink>
-      </>
-    }
-    return () => (
-      <WelcomLayout v-slots={slots}></WelcomLayout>
-    );
-  }
-})
+import { FunctionalComponent } from 'vue';
+export const First: FunctionalComponent = () => {
+  return <div class={s.card}>
+    <img src={pig} />
+    <h2>会挣钱<br />还会省钱</h2>
+  </div>
+}
 
 First.displayName = 'First'
