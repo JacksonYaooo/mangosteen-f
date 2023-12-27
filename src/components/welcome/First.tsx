0b1bfd703d1d866e022be3a30561c582ba695ca7
JacksonYaooo <2594838054@qq.com>
2023-12-27 14:29:50 +0800

    滑动切换路由

--- src/components/welcome/First.tsx
+++ src/components/welcome/First.tsx
@@ -1,13 +1,27 @@
+import { useRouter } from 'vue-router';
+import { useSwipe } from '../../hooks/useSwipe';
 import s from './welcome.module.scss';
-import { FunctionalComponent } from 'vue';
+import { defineComponent, ref, watchEffect } from 'vue';
 
-export const First: FunctionalComponent = () => {
-  return <div class={s.card}>
-    <svg>
-      <use xlinkHref='#pig'></use>
-    </svg>
-    <h2>会挣钱<br />还会省钱</h2>
-  </div>
-}
-
-First.displayName = 'First'
+export const First = defineComponent({
+  setup() {
+    const div = ref<HTMLDivElement>()
+    const router = useRouter()
+    const { swiping, direction } = useSwipe(div, {
+      beforeStart: e => e.preventDefault() // 取消浏览器默认全屏滑动
+    })
+    watchEffect(() => {
+      if (swiping.value && direction.value === 'left') {
+        router.push('/welcome/2')
+      }
+    })
+    return () => (
+      <div class={s.card} ref={div}>
+        <svg>
+          <use xlinkHref='#pig'></use>
+        </svg>
+        <h2>会挣钱<br />还会省钱</h2>
+      </div>
+    )
+  }
+})
