30db55ce052bed36f449e01847a82b36e1027c6c
JacksonYaooo <2594838054@qq.com>
2024-01-08 15:25:05 +0800

    fat: 创建Overlay组件

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -5,24 +5,32 @@ import { FloatButton } from "../shared/FloatButton";
 import { Icon } from "../shared/Icon";
 import { Center } from "../shared/Center";
 import { Navbar } from "../shared/Navbar";
+import { Overlay } from "../shared/Overlay";
 
 export const StartPage = defineComponent({
   setup(props, context) {
-    const onClick = () => {
-      console.log('hi')
+    const refOverlayVisible = ref(false)
+    const onClickMenu = () => {
+      refOverlayVisible.value = true
     }
     return () => (
       <div>
         <Navbar>{
-          { default: '山竹记账', icon: <Icon name="menu" class={s.navIcon} /> }
+          {
+            default: () => '山竹记账',
+            icon: () => <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />
+          }
         }</Navbar>
         <Center class={s.mangosteen_wrapper}>
           <Icon name="mangosteen" class={s.mangosteen} />
         </Center>
         <div class={s.button_wrapper}>
-          <Button class={s.button} onClick={onClick}>开始记账</Button>
+          <Button class={s.button} >开始记账</Button>
         </div>
         <FloatButton iconName="add" />
+        {refOverlayVisible.value &&
+          <Overlay onClose={() => refOverlayVisible.value = false} />
+        }
       </div>
     );
   }
