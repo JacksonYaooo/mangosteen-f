b8529e274cba1d9e1fc0e16087f5db85b7f1cbd5
JacksonYaooo <2594838054@qq.com>
2024-01-08 21:57:02 +0800

    feat: 打算引入vant ui

--- src/components/item/InputPad.tsx
+++ src/components/item/InputPad.tsx
@@ -1,5 +1,6 @@
-import { PropType, defineComponent } from "vue";
+import { PropType, defineComponent, ref } from "vue";
 import s from "./InputPad.module.scss";
+import { time } from '../../shared/time';
 import { Icon } from "../../shared/Icon";
 
 export const InputPad = defineComponent({
@@ -9,6 +10,8 @@ export const InputPad = defineComponent({
     }
   },
   setup(props, context) {
+    const refDate = ref<Date>()
+    const now = new Date()
     const buttons = [
       { text: '1', onClick: () => { } },
       { text: '2', onClick: () => { } },
@@ -31,7 +34,9 @@ export const InputPad = defineComponent({
       <div class={s.dateAndAmount}>
         <span class={s.date}>
           <Icon name="date" class={s.icon} />
-          <span>2024-01-01</span>
+          <span>
+            <input type="date" value={time(now).format()} />
+          </span>
         </span>
         <span class={s.amount}> 0.00 </span>
       </div>
