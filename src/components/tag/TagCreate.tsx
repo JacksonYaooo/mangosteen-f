177111ee5ab9567e55d5da47d7f1774f49c02f9a
JacksonYaooo <2594838054@qq.com>
2024-01-09 20:51:48 +0800

    feat: 完成创建标签表单功能

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -1,9 +1,10 @@
-import { ErrorCodes, PropType, defineComponent, reactive, toRaw } from "vue";
+import { PropType, defineComponent, reactive } from "vue";
 import { MainLayout } from "../../layouts/MainLayout";
 import { Icon } from "../../shared/Icon";
 import { Button } from '../../shared/Button';
 import s from './TagCreate.module.scss';
 import { EmojiSelect } from "../../shared/EmojiSelect";
+import { Rules, validate } from '../../shared/validate';
 
 export const TagCreate = defineComponent({
   props: {
@@ -16,18 +17,18 @@ export const TagCreate = defineComponent({
       name: '',
       sign: ''
     })
+    const errors = reactive<{ [k in keyof typeof formData]?: string[] }>({})
     const onSubmit = (e: Event) => {
-      console.log(toRaw(formData))
-      const rules = [
-        { key: 'name', required: true, message: '必填' },
-        { key: 'name', pattern: /^.{1,4}$/, message: '只能填1-4个字符' },
-        { key: 'sign', requierd: true }
+      const rules: Rules<typeof formData> = [
+        { key: 'name', type: 'required', message: '必填' },
+        { key: 'name', type: 'pattern', regex: /^.{1,4}$/, message: '只能填 1 到 4 个字符' },
+        { key: 'sign', type: 'required', message: '必填' },
       ]
-      // const errors = validate(formData, rules)
-      // errors = {
-      //   name:['1'],
-      //   sign:['2']
-      // }
+      Object.assign(errors, {
+        name: undefined,
+        sign: undefined
+      })
+      Object.assign(errors, validate(formData, rules))
       e.preventDefault()
     }
     return () => (
@@ -43,7 +44,7 @@ export const TagCreate = defineComponent({
                   <input v-model={formData.name} class={[s.formItem, s.input, s.error]}></input>
                 </div>
                 <div class={s.formItem_errorHint}>
-                  {/* <span>{errors['name'][0]}</span> */}
+                  <span>{errors['name'] ? errors['name'][0] : '　'}</span>
                 </div>
               </label>
             </div>
@@ -54,7 +55,7 @@ export const TagCreate = defineComponent({
                   <EmojiSelect v-model={formData.sign} class={[s.formItem, s.emojiList, s.error]} />
                 </div>
                 <div class={s.formItem_errorHint}>
-                  <span>必填</span>
+                  <span>{errors['sign'] ? errors['sign'][0] : '　'}</span>
                 </div>
               </label>
             </div>
