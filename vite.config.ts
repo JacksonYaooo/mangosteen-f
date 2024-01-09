f5d88a2bfb669b03aa1843f4ffbc508a664f30cf
JacksonYaooo <2594838054@qq.com>
2024-01-09 11:19:06 +0800

    feat: 引入vant实现日期选择

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // @ts-nocheck
 import { svgstore } from './src/vite_plugins/svgstore';
-import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueJsx from '@vitejs/plugin-vue-jsx';
+import styleImport, { VantResolve } from 'vite-plugin-style-import';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,9 +15,13 @@ export default defineConfig({
       mergeProps: true
     }),
     svgstore(),
+    styleImport({
+      resolves: [VantResolve()],
+    }),
   ],
   server: {
     host: "0.0.0.0",
     port: 3000,
-  }
+  },
+
 })
