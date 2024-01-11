afc81e90681ac0348112d6d3b9dcb5d700080749
JacksonYaooo <2594838054@qq.com>
2024-01-11 14:24:28 +0800

    wip: 创建TagEdit.tsx

--- src/components/tag/TagCreate.tsx
+++ src/components/tag/TagCreate.tsx
@@ -2,16 +2,11 @@ import { PropType, defineComponent, reactive } from "vue";
 import { MainLayout } from "../../layouts/MainLayout";
 import { Icon } from "../../shared/Icon";
 import { Button } from '../../shared/Button';
-import s from './TagCreate.module.scss';
+import s from './Tag.module.scss';
 import { EmojiSelect } from "../../shared/EmojiSelect";
 import { Rules, validate } from '../../shared/validate';
 
 export const TagCreate = defineComponent({
-  props: {
-    name: {
-      type: String as PropType<string>
-    }
-  },
   setup(props, context) {
     const formData = reactive({
       name: '',
