264d20ab66660be47f1e4471be36d7504fc4e865
JacksonYaooo <2594838054@qq.com>
2024-02-20 16:59:04 +0800

    feat: 创建统计图表页面

---
+++ src/components/statistics/Charts.tsx
@@ -0,0 +1,20 @@
+import { PropType, defineComponent } from "vue";
+import s from "./Charts.module.scss";
+
+export const Charts = defineComponent({
+  props: {
+    startDate: {
+      type: String as PropType<string>,
+      required: true
+    },
+    endDate: {
+      type: String as PropType<string>,
+      required: true
+    }
+  },
+  setup(props, context) {
+    return () => (
+      <div></div>
+    );
+  }
+})
