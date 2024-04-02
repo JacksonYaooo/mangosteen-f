612480eeb0937c2f248657eafff5e4a38156f3dd
JacksonYaooo <2594838054@qq.com>
2024-04-02 21:39:11 +0800

    feat: 删除标签

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -1,10 +1,12 @@
 import { defineComponent } from "vue";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
 import { MainLayout } from "../../layouts/MainLayout";
 import { TagForm } from "./TagForm";
 import { Button } from "../../shared/Button";
 import s from "./Tag.module.scss";
 import { BackIcon } from "../../shared/BackIcon";
+import { Dialog } from "vant";
+import { http } from "../../shared/Http";
 
 export const TagEdit = defineComponent({
   setup(props, context) {
@@ -12,6 +14,20 @@ export const TagEdit = defineComponent({
     const numberId = parseInt(route.params.id!.toString());
     if (Number.isNaN(numberId)) {
       return () => <div>id 不存在</div>;
+    }
+    const router = useRouter()
+    const onError = ()=>{
+      Dialog.alert({ title:'提示',message:'删除失败' })
+    }
+    const onDelete = async (options?: {withItems?: boolean})=>{
+      await Dialog.confirm({
+        title:'确认',
+        message:'确定删除吗？'
+      })
+      await http.delete(`/tags/${numberId}`, {
+        withItems: options?.withItems ? 'true' : 'false'
+      }).catch(onError)
+      router.back()
     }
     return () => (
       <MainLayout>
@@ -22,13 +38,17 @@ export const TagEdit = defineComponent({
             <>
               <TagForm id={numberId} />
               <div class={s.actions}>
-                <Button level="danger" class={s.removeTags} onClick={() => {}}>
+                <Button
+                  level="danger"
+                  class={s.removeTags}
+                  onClick={() => onDelete()}
+                >
                   删除标签
                 </Button>
                 <Button
                   level="danger"
                   class={s.removeTagsAndItems}
-                  onClick={() => {}}
+                  onClick={() => onDelete({ withItems: true })}
                 >
                   删除标签和记账
                 </Button>
