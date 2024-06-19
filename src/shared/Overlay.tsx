c43e18cb4becf9ea47d73be533ed2dc5af183a5d
JacksonYaooo <2594838054@qq.com>
2024-06-19 23:40:13 +0800

    feat: 完成了退出登录、显示当前用户的功能

--- src/shared/Overlay.tsx
+++ src/shared/Overlay.tsx
@@ -1,7 +1,9 @@
-import { PropType, defineComponent, ref } from "vue";
-import { RouterLink } from "vue-router";
-import { Icon } from "./Icon";
-import s from "./Overlay.module.scss";
+import { Dialog } from 'vant'
+import { defineComponent, onMounted, PropType, ref } from 'vue'
+import { RouterLink, useRoute } from 'vue-router'
+import { Icon } from './Icon'
+import { mePromise } from './me'
+import s from './Overlay.module.scss'
 
 export const Overlay = defineComponent({
   props: {
@@ -13,14 +15,35 @@ export const Overlay = defineComponent({
     const close = () => {
       props.onClose?.();
     };
-    const onClickSignIn = () => {};
+    const route = useRoute()
+    const me = ref<User>()
+    onMounted(async () => {
+      const response = await mePromise
+      me.value = response?.data.resource
+    })
+    const onSignOut = async () => {
+      await Dialog.confirm({
+        title: '确认',
+        message: '你真的要退出登录吗？',
+      })
+      localStorage.removeItem('jwt')
+    }
     return () => (
       <>
         <div class={s.mask} onClick={close}></div>
         <div class={s.overlay}>
-          <section class={s.currentUser} onClick={onClickSignIn}>
-            <h2>未登录用户</h2>
-            <p>点击这里登录</p>
+          <section class={s.currentUser}>
+            {me.value ? (
+              <div>
+                <h2 class={s.email}>{me.value.email}</h2>
+                <p onClick={onSignOut}>点击这里退出登录</p>
+              </div>
+            ) : (
+              <RouterLink to={`/sign_in?return_to=${route.fullPath}`}>
+                <h2>未登录用户</h2>
+                <p>点击这里登录</p>
+              </RouterLink>
+            )}
           </section>
           <nav>
             <ul class={s.action_list}>
@@ -46,7 +69,7 @@ export const Overlay = defineComponent({
           </nav>
         </div>
       </>
-    );
+    )
   },
 });
 
@@ -63,6 +86,6 @@ export const OverlayIcon = defineComponent({
           <Overlay onClose={() => (refOverlayVisible.value = false)} />
         )}
       </>
-    );
+    )
   },
 });
