6e16520292e8863c116425440ab811ac2bb6299e
JacksonYaooo <2594838054@qq.com>
2024-06-21 13:12:13 +0800

    feat: 完成条形图

--- src/components/statistics/Charts.tsx
+++ src/components/statistics/Charts.tsx
@@ -52,10 +52,8 @@ export const Charts = defineComponent({
       // console.log('response.data')
       // console.log(response.data)
       data1.value = response.data.groups
-      // console.log(data1.value)
     })
 
-    // data2
     const data2 = ref<Data2>([])
     const betterData2 = computed<{ name: string; value: number }[]>(() =>
       data2.value.map((item) => ({
@@ -63,6 +61,14 @@ export const Charts = defineComponent({
         value: item.amount
       }))
     )
+
+    const betterData3 = computed<{ tag: Tag, amount: number, percent: number }[]>(() => {
+      const total = data2.value.reduce((sum, item) => sum + item.amount, 0)
+      return data2.value.map(item => ({
+        ...item,
+        percent: Math.round(item.amount / total * 100)
+      }))
+    })
 
     onMounted(async () => {
       const response = await http.get<{ groups: Data2; summary: number }>('/items/summary', {
@@ -82,7 +88,7 @@ export const Charts = defineComponent({
         ]} v-model={kind.value} />
         <LineChart data={betterData1.value} />
         <PieChart data={betterData2.value} />
-        <Bars />
+        <Bars data={betterData3.value} />
       </div>
     )
   }
