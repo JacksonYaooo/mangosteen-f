6af0353594a2f3adee5f49dad22ba526c023a788
JacksonYaooo <2594838054@qq.com>
2024-06-21 13:01:11 +0800

    feat: 完成饼图

--- src/mock/mock.tsx
+++ src/mock/mock.tsx
@@ -5,23 +5,43 @@ type Mock = (config: AxiosRequestConfig) => [number, any];
 
 faker.setLocale("zh_CN");
 
-export const mockItemSummary: Mock = config => {
-  return [200, {
-    "groups": [
-      { "happen_at": "2024-06-18T00:00:00.000+0800", "amount": 100 },
-      { "happen_at": "2024-06-22T00:00:00.000+0800", "amount": 300 },
-      { "happen_at": "2024-06-29T00:00:00.000+0800", "amount": 200 }
-    ],
-    "summary": 600
-  }]
+export const mockItemSummary: Mock = (config) => {
+  if (config.params.group_by === 'happen_at') {
+    return [
+      200,
+      {
+        groups: [
+          { happen_at: '2024-06-18T00:00:00.000+0800', amount: 100 },
+          { happen_at: '2024-06-22T00:00:00.000+0800', amount: 300 },
+          { happen_at: '2024-06-29T00:00:00.000+0800', amount: 200 }
+        ],
+        summary: 600
+      }
+    ]
+  } else {
+    return [
+      200,
+      {
+        groups: [
+          { tag_id: 1, tag: { id: 1, name: '交通' }, amount: 100 },
+          { tag_id: 2, tag: { id: 2, name: '吃饭' }, amount: 300 },
+          { tag_id: 3, tag: { id: 3, name: '购物' }, amount: 200 }
+        ],
+        summary: 600
+      }
+    ]
+  }
 }
 
-export const mockItemIndexBalance: Mock = config => {
-  return [200, {
-    expenses: 9900,
-    income: 9900,
-    balance: 0
-  }]
+export const mockItemIndexBalance: Mock = (config) => {
+  return [
+    200,
+    {
+      expenses: 9900,
+      income: 9900,
+      balance: 0
+    }
+  ]
 }
 export const mockItemIndex: Mock = (config) => {
   const { kind, page } = config.params;
@@ -32,21 +52,14 @@ export const mockItemIndex: Mock = (config) => {
     per_page,
     count,
   });
-  const createTag = (attrs?: any) =>
-  ({
-    id: createId(),
-    name: faker.lorem.word(),
-    sign: faker.internet.emoji(),
-    kind: 'expenses',
-    ...attrs
-  })
+
   const createItem = (n = 1, attrs?: any) =>
     Array.from({ length: n }).map(() => ({
       id: createId(),
       user_id: createId(),
       amount: Math.floor(Math.random() * 10000),
       tags_id: [createId()],
-      tags: [createTag()],
+      // tags: [createTag()],
       happen_at: faker.date.past().toISOString(),
       kind: config.params.kind,
     }));
@@ -73,22 +86,22 @@ export const mockTagEdit: Mock = (config) => {
     id: createId(),
     name: faker.lorem.word(),
     sign: faker.internet.emoji(),
-    kind: "expenses",
-    ...attrs,
-  });
-  return [200, { resource: createTag() }];
-};
+    kind: 'expenses',
+    ...attrs
+  })
+  return [200, { resource: createTag() }]
+}
 
 export const mockTagShow: Mock = (config) => {
   const createTag = (attrs?: any) => ({
     id: createId(),
     name: faker.lorem.word(),
     sign: faker.internet.emoji(),
-    kind: "expenses",
-    ...attrs,
-  });
-  return [200, { resource: createTag() }];
-};
+    kind: 'expenses',
+    ...attrs
+  })
+  return [200, { resource: createTag() }]
+}
 
 export const mockItemCreate: Mock = (config) => {
   return [
@@ -100,14 +113,14 @@ export const mockItemCreate: Mock = (config) => {
         amount: 9900,
         note: null,
         tags_id: [3508],
-        happen_at: "2020-10-29T16:00:00.000Z",
-        created_at: "2022-07-03T15:35:56.301Z",
-        updated_at: "2022-07-03T15:35:56.301Z",
-        kind: "expenses",
-      },
-    },
-  ];
-};
+        happen_at: '2020-10-29T16:00:00.000Z',
+        created_at: '2022-07-03T15:35:56.301Z',
+        updated_at: '2022-07-03T15:35:56.301Z',
+        kind: 'expenses'
+      }
+    }
+  ]
+}
 
 export const mockSession: Mock = (config) => {
   return [
