09f3b972b4b6ca87c199f4ab4c7bf6400d9b3c0c
JacksonYaooo <2594838054@qq.com>
2024-06-27 23:05:20 +0800

    refactor: tags_id 改为 tag_ids

--- src/env.d.ts
+++ src/env.d.ts
@@ -19,17 +19,17 @@ type Tag = {
   user_id: number;
   name: string;
   sign: string;
-  kind: expenses | income;
+  kind: "expenses" | "income";
 };
 
 type Item = {
   id: number;
   user_id: number;
   amount: number;
-  tags_id: number[];
+  tag_ids: number[];
   tags?: Tag[];
   happen_at: string;
-  kind: expenses | income;
+  kind: "expenses" | "income";
 };
 
 type User = {
@@ -53,3 +53,5 @@ type Resource<T> = {
 type ResourceError = {
   errors: Record<string, string[]>;
 };
+
+type FormErrors<T> = { [K in keyof typeof T]: string[] };
--- src/shared/Tabs.tsx
+++ src/shared/Tabs.tsx
@@ -1,10 +1,9 @@
-import { PropType, defineComponent } from "vue";
-import s from "./Tabs.module.scss";
-
+import { defineComponent, PropType } from 'vue';
+import s from './Tabs.module.scss';
 export const Tabs = defineComponent({
   props: {
     classPrefix: {
-      type: String,
+      type: String
     },
     selected: {
       type: String as PropType<string>,
@@ -15,59 +14,58 @@ export const Tabs = defineComponent({
       default: false
     }
   },
-  emits: ["update:selected"],
-  setup(props, context) {
+  emits: ['update:selected'],
+  setup: (props, context) => {
     return () => {
-      const tabs = context.slots.default?.();
-      if (!tabs) return () => null;
+      const tabs = context.slots.default?.()
+      if (!tabs) return () => null
       for (let i = 0; i < tabs.length; i++) {
         if (tabs[i].type !== Tab) {
-          throw new Error("<Tabs> only accepts <Tab> as children");
+          throw new Error('<Tabs> only accepts <Tab> as children')
         }
       }
-      const cp = props.classPrefix;
-      return (
-        <div class={[s.tabs, cp + "_tabs"]}>
-          <ol class={[s.tabs_nav, cp + "_tabs_nav"]}>
-            {tabs.map((item) => (
-              <li
-                class={[
-                  item.props?.name === props.selected
-                    ? [s.selected, cp + "_selected"]
-                    : "",
-                  cp + "_tabs_nav_item",
-                ]}
-                onClick={() =>
-                  context.emit("update:selected", item.props?.name)
-                }
-              >
-                {item.props?.name}
-              </li>
-            ))}
-          </ol>
-          {props.rerenderOnSelect ?
-            <div key={props.selected}>
-              {tabs.find(item => item.props?.name === props.selected)}
-            </div> :
-            <div>
-              {tabs.map((item) => (
-                <div v-show={item.props?.name === props.selected}>{item}</div>
-              ))}
-            </div>
-          }
-        </div>
-      );
-    };
-  },
-});
+      const cp = props.classPrefix
+      return <div class={[s.tabs, cp + '_tabs']}>
+        <ol class={[s.tabs_nav, cp + '_tabs_nav']}>
+          {tabs.map(item =>
+            <li class={[
+              item.props?.value === props.selected ? [s.selected, cp + '_selected'] : '',
+              cp + '_tabs_nav_item'
+            ]}
+              onClick={() => context.emit('update:selected', item.props?.value)}
+            >
+              {item.props?.name}
+            </li>)}
+        </ol>
+        {props.rerenderOnSelect ?
+          <div key={props.selected}>
+            {tabs.find(item => item.props?.value === props.selected)}
+          </div> :
+          <div>
+            {tabs.map(item =>
+              <div v-show={item.props?.value === props.selected}>{item}</div>
+            )}
+          </div>
+        }
+      </div>
+    }
+  }
+})
 
 export const Tab = defineComponent({
   props: {
     name: {
       type: String as PropType<string>,
+      required: true
     },
+    value: {
+      type: String as PropType<string>,
+      required: true
+    }
   },
-  setup(props, context) {
-    return () => <div>{context.slots.default?.()}</div>;
-  },
-});
+  setup: (props, context) => {
+    return () => (
+      <div>{context.slots.default?.()}</div>
+    )
+  }
+})
